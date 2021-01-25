ea2925a8ea3e2a3c0bb1bfe364581f8763501979
Bit0r <nie_wang@outlook.com>
2021-01-25 10:25:15 +0800

    📦 对bfs添加dist_to

--- graphs/graph/bfs.py
+++ graphs/graph/bfs.py
@@ -1,7 +1,6 @@
 from collections import deque
 
 import networkx as nx
-from networkx.drawing.nx_agraph import to_agraph
 
 
 class BreathFirstPaths:
@@ -12,8 +11,8 @@ class BreathFirstPaths:
         """
         初始化图和起点
         """
-        self.__marked = [False] * G.number_of_nodes()
-        self.__edge_to = [None] * G.number_of_nodes()
+        self.dist_to = [None] * G.number_of_nodes()
+        self._edge_to = [None] * G.number_of_nodes()
         self.__s = s
         self.__G = G
         self.__bfs()
@@ -22,22 +21,21 @@ class BreathFirstPaths:
         """
         以s为起点，进行广度优先搜索
         """
-        q = deque()
-        self.__marked[self.__s] = True
-        q.append(self.__s)
-        while len(q) != 0:
-            v = q.popleft()
+        queue = deque()
+        self.dist_to[self.__s] = 0
+        queue.append(self.__s)
+        while queue:
+            v = queue.popleft()
             for w in self.__G[v]:
-                if not self.__marked[w]:
-                    self.__edge_to[w] = v
-                    self.__marked[w] = True
-                    q.append(w)
+                if self.dist_to[w] is None:
+                    self._edge_to[w], self.dist_to[w] = v, self.dist_to[v] + 1
+                    queue.append(w)
 
     def has_path_to(self, v: int):
         """
         是否有从s到v的路径
         """
-        return self.__marked[v]
+        return self.dist_to[v] is not None
 
     def path_to(self, v: int):
         """
@@ -48,48 +46,41 @@ class BreathFirstPaths:
         u = v
         while u != self.__s:
             path.appendleft(u)
-            u = self.__edge_to[u]
+            u = self._edge_to[u]
 
         path.appendleft(self.__s)
-        return path
-
-    @property
-    def path_tree(self):
-        """
-        返回搜索路径的生成树
-        """
-        return tuple(self.__edge_to)
+        return iter(path)
 
 
 if __name__ == "__main__":
     # 读取测试集
     G = nx.Graph()
     with open('tinyCG.txt') as f:
+        V = int(f.readline())
         E = int(f.readline())
-        s = int(f.readline())
         for i in range(E):
             G.add_edge(*map(int, f.readline().split()))
 
-    A = to_agraph(G)
+    A = nx.nx_agraph.to_agraph(G)
     A.node_attr['shape'] = 'circle'
     A.layout()
     A.draw('cg.png')
 
-    paths = BreathFirstPaths(G, s)
+    paths = BreathFirstPaths(G, 0)
 
     # 循环打印输出从s到任意v的一条简单路径
     for v in range(G.number_of_nodes()):
-        print(paths.path_to(v))
+        print(*paths.path_to(v), ':', paths.dist_to[v])
 
     # 输出搜索路径的生成树
     T = nx.Graph()
-    for v, u in enumerate(paths.path_tree):
+    for v, u in enumerate(paths._edge_to):
         # 树的根节点没有父链接，所以要排除
         if u is not None:
             # print(u, v)
             T.add_edge(u, v)
 
-    TA = to_agraph(T)
+    TA = nx.nx_agraph.to_agraph(T)
     TA.node_attr['shape'] = 'circle'
     TA.layout('dot')
     TA.draw('cg-tree.png')
--- graphs/graph/graph.py
+++ graphs/graph/graph.py
@@ -1,5 +1,4 @@
 from collections import deque
-from os import name
 
 
 class Graph:
