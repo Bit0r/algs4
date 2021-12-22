9286973cbf88f635a7fd36d426a82d752813c82c
Bit0r <nie_wang@outlook.com>
2021-12-22 20:18:38 +0800

    👌 路径压缩

--- fundamentals/union_find.py
+++ fundamentals/union_find.py
@@ -1,57 +1,55 @@
-from collections import deque
-
 from networkx import nx
 from networkx.drawing.nx_agraph import to_agraph
 
 
 class UF:
     """
-    并查算法
+    并查集
     """
-    def __init__(self, N: int):
+    def __init__(self, n: int):
         """
-        初始化N个触点
+        初始化一个有n颗树的森林
         """
-        self.__count = N
-        self.__component = list(range(N))
-        self.__size = [1] * N
+        self.__cnt = n
+        self.__forest = [-1] * n
 
     def union(self, p: int, q: int):
         """
         连接p和q
         """
-        i = self.find(p)
-        j = self.find(q)
+        root1 = self.find(p)
+        root2 = self.find(q)
 
-        # 如果p和q已经属于一个集合，则直接返回
-        if i == j:
+        # 如果p和q已经属于同一颗树，则直接返回
+        if root1 == root2:
             return
 
         # 将小集合并入大集合中
-        if self.__size[i] < self.__size[j]:
-            self.__component[i] = j
-            self.__size[j] += self.__size[i]
+        if self.__forest[root1] < self.__forest[root2]:
+            self.__forest[root1] += self.__forest[root2]
+            self.__forest[root2] = root1
         else:
-            self.__component[j] = i
-            self.__size[i] += self.__size[j]
+            self.__forest[root2] += self.__forest[root1]
+            self.__forest[root1] = root2
+
         # 集合数量减少1
-        self.__count -= 1
+        self.__cnt -= 1
 
     def find(self, p: int):
         """
-        查找p所在集合的标识符
+        查找p所在树的根
         """
-        # 使用一个队列，保存搜索路径
-        path = deque()
 
-        while p != self.__component[p]:
-            path.append(p)
-            p = self.__component[p]
+        # 查找树根
+        root = p
+        while self.__forest[root] >= 0:
+            root = self.__forest[root]
 
-        for v in path:
-            self.__component[v] = p
+        # 压缩路径
+        while self.__forest[p] >= 0:
+            self.__forest[p], p = root, self.__forest[p]
 
-        return p
+        return root
 
     def connected(self, p: int, q: int):
         """
@@ -60,35 +58,38 @@ class UF:
         return self.find(p) == self.find(q)
 
     @property
-    def count(self):
+    def cnt(self):
         """
         连通分量的数量
         """
-        return self.__count
+        return self.__cnt
 
     @property
     def forest(self):
         """
         返回归并后产生的森林
         """
-        return tuple(self.__component)
+        return tuple(self.__forest)
 
 
 if __name__ == "__main__":
     with open('tinyUF.txt') as f:
-        N = int(f.readline())
-        uf = UF(N)
-        for i in range(N):
+        n = int(f.readline())
+        uf = UF(n)
+        G = nx.Graph()
+
+        for i in range(n):
             p, q = tuple(map(int, f.readline().split()))
-            if not uf.connected(p, q):
-                uf.union(p, q)
-                print(p, q)
+            uf.union(p, q)
 
             # 画出每次联合后的森林图像
-            G = nx.Graph(list(enumerate(uf.forest)))
+            G.clear()
+            G.add_nodes_from(range(n))
+            G.add_edges_from(
+                filter(lambda pair: pair[1] > 0, enumerate(uf.forest)))
             A = to_agraph(G)
             A.node_attr['shape'] = 'circle'
             A.layout()
             A.draw(f'{i}-{p}-{q}.png')
 
-    print(uf.count, 'components')
+    print(uf.cnt, 'components')
