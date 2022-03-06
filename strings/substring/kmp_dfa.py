13bc5bc9724e17d54f0bbc5a937d0caed10cbffe
Bit0r <nie_wang@outlook.com>
2022-03-06 09:54:44 +0800

    ✨ 增加文件流匹配和多次匹配

--- strings/substring/kmp_dfa.py
+++ strings/substring/kmp_dfa.py
@@ -1,6 +1,3 @@
-import typer
-
-
 class KMP:
     def __init__(self, pattern: str, radix=127) -> None:
         self._dfa = [[0] * radix for _ in pattern]
@@ -10,7 +7,24 @@ class KMP:
             prefix = self._dfa[prefix][ord(char)]
             self._dfa[matched][ord(char)] = matched + 1
 
-    def find(self, text: str):
+    def find(self, text='', file: str = None):
+        if file is None:
+            return self._find_text(text)
+        else:
+            return self._find_file(file)
+
+    def _find_file(self, file):
+        with open(file, 'rb') as f:
+            matched, idx = 0, 0
+            for char in f.read1():
+                idx += 1
+                matched = self._dfa[matched][char]
+                if matched == len(self._dfa):
+                    return idx - matched
+            else:
+                return -1
+
+    def _find_text(self, text):
         matched = 0
         for idx, char in enumerate(text):
             matched = self._dfa[matched][ord(char)]
@@ -19,12 +33,27 @@ class KMP:
         else:
             return -1
 
+    def find_all(self, text: str = None):
+        while (x := self.find(text)) != -1:
+            yield x
+            text = text[x + 1:]
 
-def main(pattern: str, text: str):
+    def count(self, text: str):
+        return len(list(self.find_all(text)))
+
+
+def main(pattern: str, text: str = None, file: str = None):
     kmp = KMP(pattern)
-    assert kmp.find(text) == text.find(pattern)
-    print(kmp.find(text))
+    if file is None:
+        assert kmp.find(text) == text.find(pattern)
+        print(kmp.find(text),
+              list(kmp.find_all(text)),
+              kmp.count(text),
+              sep='\n')
+    else:
+        print(kmp.find(file=file))
 
 
 if __name__ == '__main__':
+    import typer
     typer.run(main)
